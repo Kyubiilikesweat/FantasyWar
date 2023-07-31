e1c58e30f594afbc7e6db271e7799abce63ccaa7
RedTex <jonas050109x@gmail.com>
2023-07-31 19:49:55 +0200

    Fix: Booster können wieder gegeben werden.

--- src/main/java/kyubii/de/fantasywar/commands/BoosterCommand.java
+++ src/main/java/kyubii/de/fantasywar/commands/BoosterCommand.java
@@ -266,10 +266,10 @@ public class BoosterCommand implements CommandExecutor {
                     String target = args[1];
                     Player tar = Bukkit.getPlayer(target);
                     if (tar != null){
-                        if (args[2].matches("[0-9]")) {
+                        if (args[2].matches("[0-9]+")) {
                             p.sendMessage(FantasyWar.getSystemPrefix() + String.valueOf("§7Du hast dem Spieler §e" + tar.getName() + " §6" + args[2] + " §7Booster gegeben."));
                             tar.sendMessage(FantasyWar.getSystemPrefix() + String.valueOf("§e" + p.getName() + " §7hat dir §6" + args[2] + " §7Booster gegeben."));
-                            BoosterClass.AddBooster(tar, Integer.parseInt(args[2]));
+                            BoosterClass.AddBooster(tar, Integer.valueOf(args[2]).intValue());
                         }else p.sendMessage(FantasyWar.getSystemPrefix() + String.valueOf("§cBitte gebe eine Zahl an."));
                     }else p.sendMessage(FantasyWar.getSystemPrefix() + String.valueOf("§cDer Spieler ist nicht online!"));
                     }else p.sendMessage(FantasyWar.getNoPerm());
